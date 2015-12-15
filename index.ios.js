9bdd199b6ec286a0703a4c6c9ec9776d85556d2e
居玉皓 <heiwu805@hotmail.com>
2015-12-15 14:22:24 +0800

    add get/set enabled

--- index.ios.js
+++ index.ios.js
@@ -7,64 +7,93 @@ var {
     View,
     AppRegistry,
     StyleSheet,
+    Text,
+    TextInput,
+    SwitchIOS,
     } = React;
 
 var xspinner = React.createClass({
-  getInitialState: function () {
-    return {
-      step: 1,
-      enabled: true
-    }
-  },
-  render: function () {
-    var t = this
-    setTimeout(function(){
-      t.setState({
-        step: 10,
-        enabled: false
-      })
-    }, 2000)
+    getInitialState: function () {
+        return {
+            step: '1',
+            enabled: true,
+            spinnerVal: '5',
+            settedSpinnerVal: '5'
+        }
+    },
+    getSpinnerValue: function(val){
+        this.setState({spinnerVal: val})
+    },
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Spinner
+                    value={this.state.spinnerVal}
+                    step={this.state.step}
+                    enabled={this.state.enabled}
+                    onChange={this.getSpinnerValue}
+                />
 
-    var step = {
-      '2015-10-1': '国庆节',
-      '2015-9-10': '教师节',
-      '2016-1-1': '元旦节',
-      '2015-11-11': '双十一'
-    };
-    var check = {
-      '2015-10-1': 'checked',
-      '2015-9-1': 'checked',
-      '2015-7-10': 'checked',
-      '2015-9-10': 'checked'
-    };
-    var headerStyle = {
-      backgroundColor: '#3C9BFD',
-      color: '#fff',
-      fontSize: 15,
-      fontWeight: '500',
-    };
-    return (
-        <View style={styles.container}>
-          <Spinner
-              touchEvent={this.press}
-              headerStyle={headerStyle}
-              step={this.state.step}
-              enabled={this.state.enabled}
-              check={check}
-          />
-        </View>
-    );
-  }
+                <View style={styles.operation}>
+                    <Text>设置step</Text>
+                    <TextInput
+                        style={[styles.opeComponent, styles.input]}
+                        onChangeText={(text) => this.setState({step: text})}
+                        value={this.state.step}
+                    />
+                </View>
+
+                <View style={styles.operation}>
+                    <Text>Set value</Text>
+                    <TextInput
+                        style={[styles.opeComponent, styles.input]}
+                        onChangeText={(text) => this.setState({spinnerVal: text})}
+                        value={String(this.state.settedSpinnerVal)}
+                    />
+                </View>
+
+                <View style={styles.operation}>
+                    <Text>Get value</Text>
+                    <Text style={styles.opeComponent}>
+                        {this.state.spinnerVal}
+                    </Text>
+                </View>
+
+                <View style={styles.operation}>
+                    <Text>设置enabled</Text>
+                    <SwitchIOS
+                        style={styles.opeComponent}
+                        onValueChange={(value) => this.setState({enabled: value})}
+                        value={this.state.enabled} />
+                </View>
+            </View>
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-    paddingTop: 30,
-    paddingLeft: 10,
-    paddingRight: 10
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#f2f2f2',
+        paddingTop: 30,
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    operation: {
+        marginTop: 20
+    },
+    opeComponent: {
+        marginTop: 5
+    },
+    input: {
+        height: 30,
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 5,
+        paddingRight: 5,
+        borderColor: 'gray',
+        borderWidth: 1
+    },
 });
 
 AppRegistry.registerComponent('xspinner', () => xspinner);
--- xspinner.js
+++ xspinner.js
@@ -1,7 +1,5 @@
 'use strict'
 
-// TODO disable/enable get/set
-
 var React = require('react-native')
 
 var {
@@ -17,8 +15,8 @@ var {
 var Calendar = React.createClass({
     getInitialState: function () {
         return {
-            val: 0,
-            activeOpacity: 1
+            value: 0,
+            activeOpacity: 1,
         }
     },
     add: function () {
@@ -28,17 +26,26 @@ var Calendar = React.createClass({
         opearte.call(this, -1)
     },
     render: function () {
-        var st = this.state,
-            pr = this.props
+        var props = this.props
+
+        this.state.value = this.props.value
 
         return (
             <View style={styles.container}>
-                <TouchableOpacity style={[styles.btn, styles.btnSub]} onPress={this.sub} activeOpacity={pr.enabled ? 0.5 : 1}>
-                    <Text style={styles.btnText}>-</Text>
+                <TouchableOpacity
+                    style={[styles.btn, styles.btnSub]}
+                    onPress={this.sub}
+                    activeOpacity={props.enabled ? 0.5 : 1}>
+                    <Text style={[styles.btnText, props.enabled ? {} : styles.btnTextDisabled]}>-</Text>
                 </TouchableOpacity>
-                <Text style={styles.content}>{this.state.val}</Text>
-                <TouchableOpacity style={[styles.btn, styles.btnAdd]} onPress={this.add} activeOpacity={pr.enabled ? 0.5 : 1}>
-                    <Text style={styles.btnText}>+</Text>
+                <Text style={styles.content}>
+                    {this.state.value}
+                </Text>
+                <TouchableOpacity
+                    style={[styles.btn, styles.btnAdd]}
+                    onPress={this.add}
+                    activeOpacity={props.enabled ? 0.5 : 1}>
+                    <Text style={[styles.btnText, props.enabled ? {} : styles.btnTextDisabled]}>+</Text>
                 </TouchableOpacity>
             </View>
         )
@@ -49,10 +56,18 @@ function opearte(direction){
     this.setState(function(previousState, currentProps) {
         if(currentProps.enabled){
             var step = parseFloat(currentProps.step),
-                maxStepDigit = 10
+                value = parseFloat(previousState.value),
+                maxStepDigit = 10,
+                result
 
+            // calculate
             step = isNaN(step) ? 1 : step
-            return {val: parseFloat((previousState.val + step * direction).toPrecision(maxStepDigit))}
+            result = parseFloat((value + step * direction).toPrecision(maxStepDigit))
+
+            // trigger onChange
+            this.props.onChange ? this.props.onChange(result) : null
+
+            return {value: result}
         }
     })
 }
@@ -71,7 +86,7 @@ var styles = StyleSheet.create({
     },
     content: {
         flex: 1,
-        color: "#00AFC7",
+        color: "#00afc7",
         textAlign: 'center',
     },
     btn: {
@@ -81,15 +96,18 @@ var styles = StyleSheet.create({
         borderColor: '#ddd',
     },
     btnText: {
-        color: "#00AFC7",
+        color: "#00afc7",
         textAlign: 'center',
+    },
+    btnTextDisabled: {
+        color: "#aaa",
     },
     btnAdd: {
         borderLeftWidth: 1,
     },
     btnSub: {
         borderRightWidth: 1,
-    }
+    },
 })
 
 module.exports = Calendar
